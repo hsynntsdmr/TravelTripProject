869ae6c9ef1a9116e2de0c4eeed3ef5df917e6d1
hsynntsdmr <hsynntsdmr@hotmail.com>
2023-12-12 17:45:27 +0300

    New changes added.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models;
+
+namespace TravelTripProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        Context context = new Context();
+        // GET: About
+        public ActionResult Index()
+        {
+            var degerler = context.Hakkimizdas.ToList();
+            return View(degerler);
+        }
+    }
+}
--- Models/yorum.cs
+++ Models/yorum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace TravelTripProject.Models
 {
     public class Yorum
     {
+        [Key]
         public int Id { get; set; }
         public string KullaniciAdi { get; set; }
         public string  Mail { get; set; }
